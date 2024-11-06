d9d95105753ea12813d45fc73dce3a8e2f9c3091
신해승 <pricia9600@naver.com>
2024-11-06 14:15:23 +0900

    .

--- Assets/Scripts/Entity/Player/PlayerInput.cs
+++ Assets/Scripts/Entity/Player/PlayerInput.cs
@@ -1,17 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-   public Vector2 MoveDirection { get; private set; }
+    public Vector2 MoveDirection { get; private set; }
 
-   void Update()
-   {
-      // 수평(horizontal) 및 수직(vertical) 입력을 받아 Vector2로 저장
-      float horizontal = Input.GetAxisRaw("Horizontal"); // 왼쪽(-1)과 오른쪽(1)
-      float vertical = Input.GetAxisRaw("Vertical");     // 아래(-1)와 위(1)
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (Mathf.Abs(horizontal) > Mathf.Abs(vertical))
+        {
+            vertical = 0;
+        }
+        else
+        {
+            horizontal = 0;
+        }
+
+        MoveDirection = new Vector2(horizontal, vertical).normalized;
         
-      MoveDirection = new Vector2(horizontal, vertical).normalized;
-   }
+    }
 }
--- Assets/Scripts/Entity/Player/PlayerMovement.cs
+++ Assets/Scripts/Entity/Player/PlayerMovement.cs
@@ -1,24 +1,47 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f; // 이동 속도
+    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private int tilePixelSize = 32;   // 타일의 픽셀 크기
+    [SerializeField] private int pixelsPerUnit = 32;   // Pixel Per Unit
 
-    private Rigidbody2D rb;
-    private PlayerInput playerInput;
+    private bool isMoving;
+    private Vector3 targetPosition;
 
-    void Awake()
+    private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        playerInput = GetComponent<PlayerInput>();
+        targetPosition = transform.position;
     }
 
-    void FixedUpdate()
+    private void Update()
     {
-        // 플레이어의 이동 방향을 받아 이동 처리
-        Vector2 movement = playerInput.MoveDirection * moveSpeed;
-        rb.velocity = movement;
+        if (!isMoving && playerInput.MoveDirection != Vector2.zero)
+        {
+            StartCoroutine(Move());
+        }
+    }
+
+    private IEnumerator Move()
+    {
+        isMoving = true;
+
+        // 타일의 픽셀 크기와 PPU에 따른 타일 유닛 크기를 계산
+        float tileUnitSize = (float)tilePixelSize / pixelsPerUnit;
+
+        // 목표 위치를 타일 유닛 크기 단위로 설정
+        targetPosition += new Vector3(playerInput.MoveDirection.x * tileUnitSize, playerInput.MoveDirection.y * tileUnitSize, 0);
+
+        // 목표 위치까지 이동
+        while ((targetPosition - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = targetPosition; // 정확히 목표 위치에 스냅
+        isMoving = false;
     }
 }
